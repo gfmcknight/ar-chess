45a5a18951e2d4d5d315ddcaca0648517cce7d5c
Graham McKnight <graham.mcknight2@gmail.com>
2018-10-13 13:03:49 -0700

    Ask for permissions at app launch.

--- app/src/main/java/com/chess_ar/handtracking/archess/MainARActivity.java
+++ app/src/main/java/com/chess_ar/handtracking/archess/MainARActivity.java
@@ -1,10 +1,16 @@
 package com.chess_ar.handtracking.archess;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.security.Permission;
+
 public class MainARActivity extends Activity {
+
+    private static int MY_CAMERA_PERMISSIONS_CODE = 811;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,6 +25,27 @@ public class MainARActivity extends Activity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[]{Manifest.permission.CAMERA},
+                    MY_CAMERA_PERMISSIONS_CODE);
+        } else {
+            gotoOpenGLActivity();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int code, String[] permissions, int[] results) {
+        // Result 0 has the camera permission
+        if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+            gotoOpenGLActivity();
+        }
+    }
+
+    private void gotoOpenGLActivity() {
+
     }
 
     /**
